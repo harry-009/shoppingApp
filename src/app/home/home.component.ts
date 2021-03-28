d3b03a08c80d999b3c61266b8f3e5ac0675ab286
unknown <harjindersinghmehta@gmail.com>
2021-03-28 22:21:36 +0530

    added loader on home component.

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -3,26 +3,32 @@ import { Product } from '../modals/product';
 import { ProductService } from '../services/product.service';
 
 @Component({
-  selector: 'app-home',
-  templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+	selector: 'app-home',
+	templateUrl: './home.component.html',
+	styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
 
-products:Product[] = [];
+	isLoading: Boolean = true;
 
-  constructor( private productService:ProductService) { }
+	products: Product[] = [];
 
-  ngOnInit(): void {
-    this.productService.getAllProducts().subscribe((data: Product[]) => this.products = data)
-    console.log(this.products);
+	constructor(private productService: ProductService) { }
 
-  this.addProduct();
+	ngOnInit(): void {
+		this.isLoading = true;
+		this.productService.getAllProducts().subscribe((data: Product[]) => {
+			this.products = data;
+			this.isLoading = false;
+		});
+		console.log(this.products);
 
-  }
+		this.addProduct();
 
-  addProduct(){
-    this.productService.postproduct();
-  }
+	}
+
+	addProduct() {
+		this.productService.postproduct();
+	}
 
 }
