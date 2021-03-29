57ff9e64ba7a8a028a663a5bb12d1b7ff9cf4dc4
unknown <harjindersinghmehta@gmail.com>
2021-03-29 23:18:27 +0530

    completed addproduct and redirect.

--- src/app/addproduct/addproduct.component.ts
+++ src/app/addproduct/addproduct.component.ts
@@ -1,53 +1,75 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup , FormBuilder, Validators } from '@angular/forms';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 import { Product } from '../modals/product';
 import { ProductService } from '../services/product.service';
 
 
 @Component({
-  selector: 'app-addproduct',
-  templateUrl: './addproduct.component.html',
-  styleUrls: ['./addproduct.component.css']
+    selector: 'app-addproduct',
+    templateUrl: './addproduct.component.html',
+    styleUrls: ['./addproduct.component.css']
 })
 export class AddproductComponent implements OnInit {
 
- addproductForm : FormGroup;
-  loading = false;
-  submitted = false;
-  returnUrl: string;
+    addproductForm: FormGroup;
+    loading = false;
+    submitted = false;
+    returnUrl: string;
 
-  constructor( private formBuilder: FormBuilder,
-    private productService: ProductService
-    ) { 
+    constructor(
+        private formBuilder: FormBuilder,
+        private productService: ProductService,
+        private router: Router
+    ) {
 
 
-    this.addproductForm = this.formBuilder.group({
-      title: ['', Validators.required],
-      category: ['', Validators.required],
-      price: ['', Validators.required],
-      image: ['', Validators.required],
-      description: ['', Validators.required],
-  });
-  this.returnUrl= "/shop";
+        this.addproductForm = this.formBuilder.group({
+            title: ['', Validators.required],
+            category: ['', Validators.required],
+            price: ['', Validators.required],
+            image: ['', Validators.required],
+            description: ['', Validators.required],
+        });
+        this.returnUrl = "/shop";
 
-  }
+    }
 
-  ngOnInit(): void {
-  }
+    ngOnInit(): void {
+    }
 
-  onSubmit(){
-    console.log("In add product component");
-    this.submitted = true;
-    // return;
-		// addNewProduct
-		let product: Product = {id:0, title: "my new product", description: "demo", price:10, image:"", category:""};
+    onSubmit() {
+        console.log("In add product component");
+        this.submitted = true;
 
-		this.productService.addNewProduct(product).subscribe(data => {
-			console.log(data);
-		});
-	}
+        //CHECK If form is valid.
+        if (!this.addproductForm.valid) {
+            return;
+        }
+
+        console.log(this.addproductForm.controls.title.value);
+        //return;
+        // addNewProduct
+        let newproduct: Product = {
+            id: 0,
+            title: this.addproductForm.controls.title.value,
+            description: this.addproductForm.controls.description.value,
+            price: this.addproductForm.controls.price.value,
+            image: this.addproductForm.controls.image.value,
+            category: this.addproductForm.controls.category.value
+        };
+
+        this.productService.addNewProduct(newproduct).subscribe(data => {
+            console.log(data);
+            
+            //redirect to shop page.
+            this.router.navigateByUrl(this.returnUrl);
+        });
+        console.log("after call to service.");
+
+    }
 
 }
-	
+
 
 
